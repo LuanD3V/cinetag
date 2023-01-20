0625ed9b813ba4eced25694575fbada431fe9178
LuanD3V <luan.h.felipe@icloud.com>
2023-01-19 21:31:45 -0300

    Adicionando api fake para os videos

--- src/components/MoviesList/index.js
+++ src/components/MoviesList/index.js
@@ -1,7 +1,16 @@
 import styles from "./MoviesList.module.css";
-import movies from "../../json/db.json";
 import Card from "../Card";
+import { useEffect, useState } from "react";
 const MoviesList = () => {
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    fetch("https://my-json-server.typicode.com/luand3v/cinetag-api/movies")
+      .then((resp) => resp.json())
+      .then((data) => {
+        setMovies(data);
+      });
+  }, []);
+
   return (
     <section className={styles.moviesSection}>
       {movies.map((movie) => {
--- src/components/Player/index.js
+++ src/components/Player/index.js
@@ -1,12 +1,23 @@
 import Banner from "components/Banner";
 import PageNotFound from "components/PageNotFound";
 import Title from "components/Title";
-import movies from "json/db.json";
 import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+
 import styles from "./Player.module.css";
 
 const Player = () => {
   const parametros = useParams();
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    fetch(
+      `https://my-json-server.typicode.com/luand3v/cinetag-api/movies?id=${parametros.id}`
+    )
+      .then((resp) => resp.json())
+      .then((data) => {
+        setMovies(data);
+      });
+  }, []);
 
   const movie = movies.find((movie) => {
     return movie.id === Number(parametros.id);
